2221f4e2fa5c061fbb15d20c0bd4f77b6ba66ff0
Iit Pranata <iitpranataip@gmail.com>
2018-05-18 11:33:11 +0700

    Tambah OWL-CAROUSEL

---
+++ assets/custom.js
@@ -0,0 +1,9 @@
+$('.owl-carousel').owlCarousel({
+    animateOut: 'fadeOut',
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:6000,
+    autoplayHoverPause:false,
+    dots: false,
+})
