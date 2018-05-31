1931bd5fcb6c694fd11f92d23d8ac2753997956c
Iit Pranata <iitpranataip@gmail.com>
2018-05-31 12:06:48 +0700

    header dan navbar

--- assets/custom.js
+++ assets/custom.js
@@ -1,9 +1,19 @@
-$('.owl-carousel').owlCarousel({
+$('.pengumuman').owlCarousel({
     animateOut: 'fadeOut',
     items:1,
     loop:true,
     autoplay:true,
     autoplayTimeout:6000,
+    autoplayHoverPause:false,
+    dots: false,
+})
+
+$('.berita').owlCarousel({
+    animateOut: 'fadeOut',
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:8000,
     autoplayHoverPause:false,
     dots: false,
 })
